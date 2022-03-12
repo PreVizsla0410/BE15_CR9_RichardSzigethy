3767e4300171e80d778af39005ec16a308eb4bb8
PreVizsla0410 <richardszigety@gmail.com>
2022-03-12 17:55:41 +0100

    Final version

--- be15_cr9_famazon_richardszigethy (4).sql
+++ be15_cr9_famazon_richardszigethy (4).sql
@@ -193,3 +193,29 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- shows items where the price is under 50
+ 
+SELECT * FROM item WHERE price <="50";
+ 
+--2
+ 
+-- shows the sum of my orders
+ 
+SELECT SUM(sum) FROM orders;
+ 
+226
+ 
+-- shows the average sum of my orders
+ 
+SELECT AVG(sum)
+FROM orders;
+ 
+75.3333
+ 
+--adding together the orders ID´s and customer orders
+ 
+select customer_ID, First_Name, Last_Name, address from customers join item on item_id = customer_ID;
+ 
+--adding together order and shipping lists
+ 
+select order_ID, customer_order, fk_shipping_ID, fk_customer_ID, order_ID from orders join shipping on shipping_id = fk_shipping_ID;
